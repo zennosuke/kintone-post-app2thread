04d783d9d3e93f0f437a759ff129b6aaab645ddf
zennosuke <zen.e.0719@gmail.com>
2020-04-13 21:42:31 +0900

    Initial commit

--- src/js/config.js
+++ src/js/config.js
@@ -3,22 +3,137 @@ jQuery.noConflict();
 (function($, PLUGIN_ID) {
   'use strict';
 
-  var $form = $('.js-submit-settings');
-  var $cancelButton = $('.js-cancel-button');
-  var $message = $('.js-text-message');
-  var config = kintone.plugin.app.getConfig(PLUGIN_ID);
+  const fieldElements = {
+    $form: $('.js-submit-settings'),
+    $cancelButton: $('.js-cancel-button'),
+    $userid: $('.js-text-userid'),
+    $password: $('.js-text-password'),
+    $space: $('.js-text-space'),
+    $thread: $('.js-text-thread'),
+    $mentionCode: $('.js-text-mention-code'),
+    $mentionType: document.getElementsByName('radio'),
+    $fields: $('.js-dropdown-field'),
+    $checkboxCode: $('.js-text-checkbox-code'),
+    $checkboxOption: $('.js-text-checkbox-option')
+  };
 
-  if (config.message) {
-    $message.val(config.message);
+  const config = kintone.plugin.app.getConfig(PLUGIN_ID);
+
+  // ラジオボタンのデータを取得
+  function getMentionType() {
+    let mentionType = '';
+    const radioButton = document.getElementsByName('radio');
+
+    radioButton.forEach(el => {
+      if (el.checked === true) {
+        mentionType = el.value;
+      }
+    });
+    return mentionType;
   }
-  $form.on('submit', function(e) {
+
+  // スレッドに投稿するフィールドを選択するドロップダウンを生成
+  // eslint-disable-next-line no-undef
+  KintoneConfigHelper.getFields()
+    .then((resp) => {
+      // ドロップダウンの一行目に未選択の選択肢を設置
+      let $option = $('<option></option>');
+      $option.attr('value', '');
+      $option.text('---');
+      fieldElements.$fields.append($option);
+
+      resp.forEach(field => {
+        $option = $('<option></option>');
+        $option.attr('value', `${field.code}&${field.type}&${field.label}`);
+        $option.text(field.label);
+        fieldElements.$fields.append($option);
+      });
+    })
+    .catch((err) => {
+      // console.log(err);
+    });
+
+  // 既に保持している設定情報を反映
+  if (config.userid) {
+    fieldElements.$userid.val(config.userid);
+  }
+  if (config.password) {
+    fieldElements.$password.val(config.$password);
+  }
+  if (config.space) {
+    fieldElements.$space.val(config.space);
+  }
+  if (config.thread) {
+    fieldElements.$thread.val(config.thread);
+  }
+  if (config.mentionCode) {
+    fieldElements.$mentionCode.val(config.mentionCode);
+  }
+  if (config.mentionType) {
+    fieldElements.$mentionType.forEach(button => {
+      if (button.value === config.mentionType) button.checked = true;
+    });
+  }
+  // TODO: for文で書き直す
+  // TODO: 再度開いた際にドロップダウンの初期値が入らない問題を解決する
+  if (config.field_1_code) {
+    fieldElements.$fields[0].value = `${config.field_1_code}&${config.field_1_type}&${config.field_1_label}`;
+  }
+  if (config.field_2_code) {
+    fieldElements.$fields[1].value = `${config.field_2_code}&${config.field_2_type}&${config.field_2_label}`;
+  }
+  if (config.field_3_code) {
+    fieldElements.$fields[2].value = `${config.field_3_code}&${config.field_3_type}&${config.field_3_label}`;
+  }
+  if (config.field_4_code) {
+    fieldElements.$fields[3].value = `${config.field_4_code}&${config.field_4_type}&${config.field_4_label}`;
+  }
+  if (config.field_5_code) {
+    fieldElements.$fields[4].value = `${config.field_5_code}&${config.field_5_type}&${config.field_5_label}`;
+  }
+  if (config.checkboxCode) {
+    fieldElements.$checkboxCode.val(config.checkboxCode);
+  }
+  if (config.checkboxOption) {
+    fieldElements.$checkboxOption.val(config.checkboxOption);
+  }
+
+  fieldElements.$form.on('submit', (e) => {
     e.preventDefault();
-    kintone.plugin.app.setConfig({message: $message.val()}, function() {
+
+    const configSettings = {
+      userid: fieldElements.$userid.val(),
+      password: fieldElements.$password.val(),
+      space: fieldElements.$space.val(),
+      thread: fieldElements.$thread.val(),
+      mentionCode: fieldElements.$mentionCode.val() || '',
+      mentionType: getMentionType(),
+      field_1_code: fieldElements.$fields[0].value.split('&')[0] || '',
+      field_1_type: fieldElements.$fields[0].value.split('&')[1] || '',
+      field_1_label: fieldElements.$fields[0].value.split('&')[2] || '',
+      field_2_code: fieldElements.$fields[1].value.split('&')[0] || '',
+      field_2_type: fieldElements.$fields[1].value.split('&')[1] || '',
+      field_2_label: fieldElements.$fields[1].value.split('&')[2] || '',
+      field_3_code: fieldElements.$fields[2].value.split('&')[0] || '',
+      field_3_type: fieldElements.$fields[2].value.split('&')[1] || '',
+      field_3_label: fieldElements.$fields[2].value.split('&')[2] || '',
+      field_4_code: fieldElements.$fields[3].value.split('&')[0] || '',
+      field_4_type: fieldElements.$fields[3].value.split('&')[1] || '',
+      field_4_label: fieldElements.$fields[3].value.split('&')[2] || '',
+      field_5_code: fieldElements.$fields[4].value.split('&')[0] || '',
+      field_5_type: fieldElements.$fields[4].value.split('&')[1] || '',
+      field_5_label: fieldElements.$fields[4].value.split('&')[2] || '',
+      checkboxCode: fieldElements.$checkboxCode.val(),
+      checkboxOption: fieldElements.$checkboxOption.val()
+    };
+
+    // TODO: ボタン押下時にuseridとpasswordが正しいか確認する仕組みを入れる
+    kintone.plugin.app.setConfig(configSettings, () => {
       alert('The plug-in settings have been saved. Please update the app!');
       window.location.href = '../../flow?app=' + kintone.app.getId();
     });
   });
-  $cancelButton.on('click', function() {
+  fieldElements.$cancelButton.on('click', () => {
     window.location.href = '../../' + kintone.app.getId() + '/plugin/';
   });
 })(jQuery, kintone.$PLUGIN_ID);
--- src/js/desktop.js
+++ src/js/desktop.js
@@ -1,24 +1,116 @@
 jQuery.noConflict();
 
-(function($, PLUGIN_ID) {
+(($, PLUGIN_ID) => {
   'use strict';
 
-  kintone.events.on('app.record.index.show', function() {
-    var config = kintone.plugin.app.getConfig(PLUGIN_ID);
+  const PLUGIN_CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
+  const POST_URL = `https://${window.location.hostname}/k/v1/space/thread/comment.json`;
 
-    var spaceElement = kintone.app.getHeaderSpaceElement();
-    var fragment = document.createDocumentFragment();
-    var headingEl = document.createElement('h3');
-    var messageEl = document.createElement('p');
-
-    messageEl.classList.add('plugin-space-message');
-    messageEl.textContent = config.message;
-    headingEl.classList.add('plugin-space-heading');
-    headingEl.textContent = 'Hello kintone plugin!';
-
-    fragment.appendChild(headingEl);
-    fragment.appendChild(messageEl);
-    spaceElement.appendChild(fragment);
+  // レコード再利用時に、投稿済フラグを初期化
+  kintone.events.on('app.record.create.show', (event) => {
+    event.record[PLUGIN_CONFIG.checkboxCode].value = [];
+    return event;
   });
 
+  // レコード登録、編集時に投稿済フラグを編集不可にする
+  kintone.events.on(['app.record.create.show', 'app.record.edit.show'], (event) => {
+    event.record[PLUGIN_CONFIG.checkboxCode].disabled = true;
+    return event;
+  });
+
+  kintone.events.on('app.record.create.submit', (event) => {
+    return new kintone.Promise((resolve, reject) => {
+      const record = event.record;
+
+      // 未対応のフィールド：RICH_TEXT,CHECK_BOX,MULTI_SELECT,FILE,USER_SELECT,SUBTABLE,ORGANIZATION_SELECT,GROUP_SELECT
+      // フィールドの値を取得する関数
+      const getFieldValue = (fieldType, fieldCode) => {
+        if (fieldType === 'CREATOR' || fieldType === 'MODIFIER') {
+          return record[fieldCode].value.name;
+        } else if (fieldType === 'DATETIME' || fieldType === 'CREATED_TIME' || fieldType === 'UPDATED_TIME') {
+          return record[fieldCode].value.replace('T', ' ').replace('Z', '');
+        }
+        return record[fieldCode].value;
+      };
+
+      // スレッドに投稿するコメントを整形する関数
+      const formatPostComment = (fields) => {
+        const postFields = fields.filter(field => field.label.length > 0);
+        const postCommentArr = [];
+        postFields.forEach(field => {
+          postCommentArr.push(`${field.label}：${getFieldValue(field.type, field.code)}`);
+        });
+        return postCommentArr.join('\n');
+      };
+
+      const FIELDS = [
+        {
+          label: PLUGIN_CONFIG.field_1_label,
+          code: PLUGIN_CONFIG.field_1_code,
+          type: PLUGIN_CONFIG.field_1_type
+        },
+        {
+          label: PLUGIN_CONFIG.field_2_label,
+          code: PLUGIN_CONFIG.field_2_code,
+          type: PLUGIN_CONFIG.field_2_type
+        },
+        {
+          label: PLUGIN_CONFIG.field_3_label,
+          code: PLUGIN_CONFIG.field_3_code,
+          type: PLUGIN_CONFIG.field_3_type
+        },
+        {
+          label: PLUGIN_CONFIG.field_4_label,
+          code: PLUGIN_CONFIG.field_4_code,
+          type: PLUGIN_CONFIG.field_4_type
+        },
+        {
+          label: PLUGIN_CONFIG.field_5_label,
+          code: PLUGIN_CONFIG.field_5_code,
+          type: PLUGIN_CONFIG.field_5_type
+        }
+      ];
+
+      const POST_COMMENT = formatPostComment(FIELDS);
+      const AUTH_STRING = btoa(`${PLUGIN_CONFIG.userid}:${PLUGIN_CONFIG.password}`);
+      const SPACE_ID = PLUGIN_CONFIG.space;
+      const THREAD_ID = PLUGIN_CONFIG.thread;
+      const MENTION_CODE = PLUGIN_CONFIG.mentionCode;
+      const MENTION_TYPE = PLUGIN_CONFIG.mentionType;
+      const BODY = {
+        space: SPACE_ID,
+        thread: THREAD_ID,
+        comment: {
+          text: POST_COMMENT,
+          mentions: [{
+            code: MENTION_CODE,
+            type: MENTION_TYPE
+          }]
+        },
+        __REQUEST_TOKEN__: kintone.getRequestToken()
+      };
+
+      // kintone.apiでは認証ユーザーの指定ができないため、
+      // プラグインに持たせた認証情報でPOSTするために fetch API を利用する
+      fetch(POST_URL, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Cybozu-Authorization': AUTH_STRING
+        },
+        body: JSON.stringify(BODY),
+      })
+        .then(() => {
+          // alert('本レコードの内容がスレッドに投稿されました。');
+          record[PLUGIN_CONFIG.checkboxCode].value[0] = PLUGIN_CONFIG.checkboxOption;
+          resolve(event);
+        })
+        .catch((resp) => {
+          // TODO: 失敗した際に、アラートを出す
+          event.error = resp.message;
+          reject(event);
+        });
+    });
+  });
 })(jQuery, kintone.$PLUGIN_ID);
